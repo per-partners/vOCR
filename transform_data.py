a240099c269e026de7c8a79c79db6f9650d0d951
Ahmed Mustafa <ahmed.mustafa@perpartners.com>
2025-03-09 01:04:14 +0200

    update the script of transformation for our dataset

--- transform_data.py
+++ transform_data.py
@@ -5,15 +5,224 @@ import shutil
 import random
 from sklearn.model_selection import train_test_split
 import cv2
-import numpy as np 
+import numpy as np
+import json
+import re
+from typing import AnyStr, List, Optional
+from collections import Counter
+
+from charset_normalizer.utils import is_arabic
+from pyarabic.normalize import normalize_searchtext
 
 
-def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
+classes = {
+    "medical provider name": [
+        "Medical  Provider",
+        "Medical Provider",
+        "medical provider"
+    ],
+    
+    "national id": [
+        "national id",
+        "National ID"
+    ],
+    
+    "employer name": [
+        "employer name",
+        "Employer Name"
+    ],
+    
+    "mobile number": [
+        "mobile number",
+        "Mobile Number",
+        "Mobile number"
+    ],
+    
+    "claim ID": [
+        "Digital",
+        "digital"
+    ],
+    
+    "stamp": [
+        "STAMPS",
+        "Stamps",
+        "stamp",
+        "Stamp",
+        "STAMP"
+    ],
+    
+    "name": [
+        "name",
+        "names",
+        "Names",
+        "Name",
+        "NAMES"
+    ],
+    
+    "drug": [
+        "Drug",
+        "drug",
+        "drugs",
+        "Drugs",
+        "DRUGS",
+        "DRUG"
+    ],
+    
+    "general handwritten": [
+        "Hand Written",
+        "H.W GENERAL",
+        "H.w.general",
+        "H.W general",
+        "general handwritten",
+        "genaral handwritten",
+        "general handwirtten",
+        "General Handwritten",
+        "Genaral Handwritten",
+        "Written",
+        "General handwritten",
+        "H.W",
+        "WRITTEN",
+        "H.w general",
+        "WR",
+
+    ],
+    
+    "number": [
+        "Number",
+        "Number",
+        "number",
+    ],
+    
+    "unknown": [
+        "UNK",
+        "unk",
+        "Uknown",
+        "Unknown",
+        "unknown"
+    ],
+    
+    "medical information": [
+        "age",
+        "Depart.",
+        "depart",
+        "address",
+        "Approval No",
+        "approval no",
+        "approval no.",
+        "depart",
+        "depart.",
+        "weight"
+    ],
+    
+    "diagnosis": [
+        "DIAGNOSIS",
+        "diagnosis",
+        "dign.",
+        "Diagnose",
+        "Diagnosis",
+        "diganosis"
+    ],
+    
+    "test": [
+        "TESTS",
+        "Tests",
+        "tests",
+        "Test",
+        "test",
+        "teste",
+    ],
+    
+    "scan": [
+        "scan",
+        "Scan"
+    ],
+    
+    "signature": [
+        "doctor signsture",
+        "doctor signature",
+        "Doctor Signature",
+        "doctor Signature",
+        "doctor singnature",
+        "Signature",
+        "signature"
+    ],
+    
+    "instructions": [
+        "Instructios",
+        "Instrutions",
+        "Insturctions",
+        "instructions",
+        "INSTRUCTIONS",
+        "instruction",
+        "insturctions",
+        "inst",
+        "Instructions",
+        "Instruction",
+        "instructons",
+        "Instrutions",
+    ],
+
+    "drug instructions": [
+        "Drug Instructions",
+        "drug instructions",
+        "Drug Insructions",
+        "Drug Instructions",
+        "Drug Instuctions",
+    ],
+    
+    "price": [
+        "Price",
+        "price"
+    ],
+
+    "date": [
+        "date",
+        "dates",
+        "Date",
+        "DATES",
+        "Dates"
+    ],
+    
+    "dental symbol": [
+        "dental symbol plus",
+        "dental symbol u.l",
+        "dental symbol u.r",
+        "dental symbol l.l",
+        "dental symbol l.r",
+        "dental symbol T"
+    ],
+    
+    "medical id": [
+        "Medical ID",
+        "medical ID"
+    ]
+}
+
+
+# Preprocess classes to create a normalized lookup
+normalized_classes = {}
+for canonical, variants in classes.items():
+    for variant in variants:
+        normalized_classes[variant.lower()] = canonical
+
+
+def normalize_class_title(class_title: str) -> str:
+    """Normalize the class title to its canonical form."""
+    return normalized_classes.get(class_title.lower(), class_title)
+
+
+def check_for_valid_claim_id(claim_id: str) -> bool:
+    """Check if the claim ID is valid."""
+    # create regex pattern for claim ID mix between digits and letters
+    pattern = re.compile(r"^[a-zA-Z0-9]*$")
+    return bool(pattern.match(claim_id))
+
+def duplicate_and_split_dataset(parent_dir, num_duplicates=50, train_size=0.7, seed=42):
     images_dir = os.path.join(parent_dir, "images")
     labels_dir = os.path.join(parent_dir, "annotation")
     dataset_root = os.path.join(parent_dir, "processed_dataset")
 
-    splits = ["train", "valid"]
+    splits = ["train", "valid"]  # "test"]
 
     # Create directories for each split
     for split in splits:
@@ -24,7 +233,8 @@ def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
     count = 1
     # Collect all samples
     for split, split_name in tqdm(
-        zip(train_test_split(image_names, test_size=1 - train_size, random_state=seed), splits),
+        zip(train_test_split(image_names, test_size=1 -
+            train_size, random_state=seed), splits),
         desc="Splitting dataset",
         colour="red",
     ):
@@ -38,21 +248,30 @@ def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
                 label = json.load(f)
 
             image = cv2.imread(image_path)
-            
+
             # Claim level
             sample = {
                 "image_name": image_name,
                 "image": image,
-                "prefix": "extract data in JSON format",
+                "prefix": "Describe the handwritten text in the image in JSON Format",
                 "suffix": {},
             }
 
             do_we_need_to_skip_the_whole_image = False
             for obj in label["objects"]:
                 class_name = obj["classTitle"]
-                if class_name == "UNK":
+                class_name = normalize_class_title(class_name)
+                if class_name == "Claim ID":
+                    # if the claim ID is not all digits, skip this object
+                    # sometimes the claim id is mix between digits and letters so we need to take this
+                    # into consideration                    
+                    if check_for_valid_claim_id(obj["description"]):
+                        continue
+                    
+                if class_name == "unknown":
                     do_we_need_to_skip_the_whole_image = True
                     break
+                
                 text = obj["description"]
                 if class_name in sample["suffix"]:
                     sample["suffix"][class_name].append(text)
@@ -63,7 +282,7 @@ def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
             if not do_we_need_to_skip_the_whole_image:
                 all_samples.append(sample)
 
-            ## Crop Level
+            # Crop Level
             for obj in label["objects"]:
                 crop_name = (
                     image_name
@@ -75,10 +294,18 @@ def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
                 )
 
                 class_name = obj["classTitle"]
-                if class_name == "UNK":
+                class_name = normalize_class_title(class_name)
+                if class_name == "unknown":
                     continue
-
+                
+                # if the claim ID is not all digits, skip this object
+                # sometimes the claim id is mix between digits and letters so we need to take this
+                # into consideration                    
+                if check_for_valid_claim_id(obj["description"]):
+                    continue
+                
                 geo_type = obj["geometryType"]
+                
                 bbox = obj["points"]["exterior"]
                 if geo_type == "polygon":
                     # convert polygon to rectangle
@@ -90,32 +317,29 @@ def duplicate_and_split_dataset(parent_dir, train_size=0.7, seed=42):
                     bbox = [x_min, y_min, x_max, y_max]
 
                 bbox = np.array(bbox).flatten().tolist()
-                
+
                 sample_crop = {
                     "image_name": crop_name,
-                    "image": image[bbox[1] : bbox[3], bbox[0] : bbox[2]],
+                    "image": image[bbox[1]: bbox[3], bbox[0]: bbox[2]],
                     "prefix": "Describe the handwritten text in the image in JSON Format.",
                     "suffix": {},
                 }
-
                 sample_crop["suffix"][class_name] = [obj["description"]]
                 all_samples.append(sample_crop)
-    
-    
 
         with open(f"{os.path.join(dataset_root, split_name)}/annotations.jsonl", "w") as f_out:
             for sample in tqdm(
                 all_samples, desc=f"Creating {split_name} dataset", colour="blue"
             ):
-                new_image_name = f"{count:06d}.jpg"  # Saves images as '000001.jpg', '000002.jpg', etc.
-                dst_image = os.path.join(dataset_root, split_name, new_image_name)
+                # Saves images as '000001.jpg', '000002.jpg', etc.
+                new_image_name = f"{count:06d}.jpg"
+                dst_image = os.path.join(
+                    dataset_root, split_name, new_image_name)
                 cv2.imwrite(dst_image, sample["image"])
                 sample["image_name"] = new_image_name
                 sample.pop("image")
                 f_out.write(json.dumps(sample) + "\n")
                 count += 1
-
-                
 
     # # Split samples
     # train_val_samples, test_samples = train_test_split(
